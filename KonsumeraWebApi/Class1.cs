01d413e14b345ac5452a3c197bdece3b4b283d50
alehag97 <alex.hagstrom@hotmail.com>
2024-12-02 17:59:27 +0100

    Add project files.

---
+++ KonsumeraWebApi/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KonsumeraWebApi
+{
+    public class Class1
+    {
+        [property: JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [property: JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [property: JsonPropertyName("html_url")]
+        public string HtmlUrl { get; set; }
+
+        [property: JsonPropertyName("homepage")]
+        public string Homepage { get; set; }
+
+        [property: JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [property: JsonPropertyName("pushed_at")]
+        public DateTime PuashedAt { get; set; }
+    }
+}
---
+++ KonsumeraWebApi/Program.cs
@@ -0,0 +1,25 @@
+﻿using KonsumeraWebApi;
+using System.Text.Json;
+
+HttpClient client = new()
+{
+    BaseAddress = new Uri("https://api.github.com/")
+};
+
+client.DefaultRequestHeaders.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; " + "Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+var response = await client.GetAsync("orgs/dotnet/repos");
+var content = await response.Content.ReadAsStringAsync();
+
+var repon = JsonSerializer.Deserialize<List<Class1>>(content);
+
+foreach (var repo in repon)
+{
+    Console.WriteLine($"Name: {repo.Name}");
+    Console.WriteLine($"Homepage: {repo.Homepage}");
+    Console.WriteLine($"GitHub: {repo.HtmlUrl}");
+    Console.WriteLine($"Description: {repo.Description}");
+    Console.WriteLine($"Watchers: {repo.Watchers}");
+    Console.WriteLine($"Last push: {repo.PuashedAt:yyyy-MM-dd HH:mm:ss}");
+    Console.WriteLine();
+}
